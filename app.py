fde2a151dd841f4bb3b42e7be1461f96c23f038b
Janeen Scott <janeen.e.scott@gmail.com>
2019-01-18 16:28:41 -0500

    pulled repo names from api

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-import csv
 
 from flask import Flask
-from flask import request
 from flask import render_template
+import requests
 
 app = Flask(__name__)
 
@@ -11,18 +10,31 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    user_data = requests.get(https://api.github.com/users/<username>)
-    translated = user_data.json()
+    user_data = requests.get('https://api.github.com/users/janeenscott') # api for main github data
+    translated = user_data.json() # translated is a dictionary (call it with brackets)
+    # translated is a dictionary
+
+    repo_content = requests.get('https://api.github.com/users/janeenscott/repos') # api for repo data
+    translated_repo = repo_content.json() # translated_repo is a list of dictionaries. 'repos' in
+                                          # context below contains a list of dictionaries now. So I can
+                                          # call dictionary items from the list through dot notation
+                                          # in my index.html file
 
     context = {
         'name': translated['name'],
-        'username':translated['login']
+        'username': translated['login'],
         'bio': translated['bio'],
         'location': translated['location'],
         'email': translated['email'],
-        'avatar_url': translated['avatar_url'],
-        'repos_url': translated['repos_url']
+        'bio_image': translated['avatar_url'],
+        'repos_url': translated['repos_url'],
+        'repos': translated_repo
     }
+    # this is saying, 'name' is now a variable that is pulling from a key value pair in the dictionary, translated
+    # so translated['name'] pulls the value from the key 'name' in the dictionary translated, and puts it in a
+    # new variable, 'name'. Now 'name' will be populated with API data from translated.
 
     return render_template('index.html', **context)
-
+                                            # had repos=translated_repos below when I thought I could
+                                            # have two functions under one app route. So I put it in the
+                                            # context so it can be accessed and put through index file
