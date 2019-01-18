ef210ba984578ecf6f4f5b9ba3eaa6d8ffca569e
Janeen Scott <janeen.e.scott@gmail.com>
2019-01-17 20:16:46 -0500

    added file

---
+++ app.py
@@ -0,0 +1,16 @@
+import csv
+
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+
+
+
+@app.route("/")
+def index():
+
+    return render_template('index.html', )
+
