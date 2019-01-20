2fab4184db19d6940f8a9ad7f5ab6e187fe18610
Janeen Scott <janeen.e.scott@gmail.com>
2019-01-20 15:57:14 -0500

    added followers route and successfully pulled in base html api info - picture, name, etc

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import requests
 app = Flask(__name__)
 
 
-
-
 @app.route("/")
 def index():
     user_data = requests.get('https://api.github.com/users/janeenscott') # api for main github data
@@ -19,7 +17,6 @@ def index():
                                           # context below contains a list of dictionaries now. So I can
                                           # call dictionary items from the list through dot notation
                                           # in my index.html file
-
     context = {
         'name': translated['name'],
         'username': translated['login'],
@@ -38,3 +35,26 @@ def index():
                                             # had repos=translated_repos below when I thought I could
                                             # have two functions under one app route. So I put it in the
                                             # context so it can be accessed and put through index file
+
+
+@app.route('/followers/')
+def followers():
+
+    user_data = requests.get('https://api.github.com/users/janeenscott')
+    translated = user_data.json()
+
+    follower_content = requests.get('https://api.github.com/users/janeenscott/followers')
+    translated_followers = follower_content.json()
+
+    context = {
+        'name': translated['name'],
+        'username': translated['login'],
+        'bio': translated['bio'],
+        'location': translated['location'],
+        'email': translated['email'],
+        'bio_image': translated['avatar_url'],
+        'repos_url': translated['repos_url'],
+        'followers': translated_followers
+    }
+
+    return render_template('followers.html', **context)
